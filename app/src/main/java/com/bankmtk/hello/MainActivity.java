08db0287837877a16789454dd2ba37d7d0e7a0a8
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-28 00:59:01 +0300

    Main activity conditionals for greeting

--- app/src/main/java/com/bankmtk/hello/MainActivity.java
+++ app/src/main/java/com/bankmtk/hello/MainActivity.java
@@ -3,6 +3,9 @@ package com.bankmtk.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView greeting = (TextView)findViewById(R.id.greeting);
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        if (5<= currentHour && currentHour<12){
+            greeting.setText("Good morning Holland");
+        }else if (12<= currentHour && currentHour < 6 ){
+            greeting.setText("Good afternoon Holland");
+        }else if (6<=currentHour && currentHour <9){
+            greeting.setText("Good evening Holland");
+        }else{
+            greeting.setText("Good night Holland");
+        }
     }
 }
