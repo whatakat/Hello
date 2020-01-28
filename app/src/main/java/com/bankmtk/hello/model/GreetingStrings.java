c7a61b26c2da3a09bf189ac3a4b6a607cf214fb5
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-28 19:41:24 +0300

    add interface GreetingStrings

---
+++ app/src/main/java/com/bankmtk/hello/model/GreetingStrings.java
@@ -0,0 +1,9 @@
+package com.bankmtk.hello.model;
+
+public interface GreetingStrings {
+    String getHelloer();
+    String getMorning();
+    String getAfternoon();
+    String getEvening();
+    String getNight();
+}
