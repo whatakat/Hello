5396f6823b5736e28cbcf0136510893b545f7207
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-28 19:34:20 +0300

    refactor

--- app/src/main/java/com/bankmtk/hello/MainActivity.java
+++ app/src/main/java/com/bankmtk/hello/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.bankmtk.hello.model.BuilderGreetingPhrase;
+
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,22 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView greeting = (TextView)findViewById(R.id.greeting);
-        greeting.setText(greetPhrase());
-    }
-    private String greetPhrase(){
-        TextView greeting = (TextView)findViewById(R.id.greeting);
-        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-        Resources resources = getResources();
-        String helloer  =resources.getString(R.string.helloer);
-        if (5<= currentHour && currentHour<12){
-            return String.format("%s %s!",resources.getString(R.string.morning),helloer);
-        }else if (12<= currentHour && currentHour < 6 ){
-            return String.format("%s %s!",resources.getString(R.string.afternoon),helloer);
-        }else if (6<=currentHour && currentHour <9){
-            return String.format("%s %s!",resources.getString(R.string.evening),helloer);
-        }else{
-            return String.format("%s %s!",resources.getString(R.string.night),helloer);
-        }
+        BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(getResources());
+        greeting.setText(builderGreetingPhrase.get());
+
 
 
     }
