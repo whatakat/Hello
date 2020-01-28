e4c44e028ce374a8983de71d8b83c6ab95e12a28
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-28 19:47:25 +0300

    add implementation

--- app/src/main/java/com/bankmtk/hello/MainActivity.java
+++ app/src/main/java/com/bankmtk/hello/MainActivity.java
@@ -7,10 +7,11 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.bankmtk.hello.model.BuilderGreetingPhrase;
+import com.bankmtk.hello.model.GreetingStrings;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GreetingStrings {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +20,30 @@ public class MainActivity extends AppCompatActivity {
         TextView greeting = (TextView)findViewById(R.id.greeting);
         BuilderGreetingPhrase builderGreetingPhrase = new BuilderGreetingPhrase(getResources());
         greeting.setText(builderGreetingPhrase.get());
+    }
 
+    @Override
+    public String getHelloer() {
+        return getResources().getString(R.string.helloer);
+    }
 
+    @Override
+    public String getMorning() {
+        return getResources().getString(R.string.morning);
+    }
 
+    @Override
+    public String getAfternoon() {
+        return getResources().getString(R.string.afternoon);
+    }
+
+    @Override
+    public String getEvening() {
+        return getResources().getString(R.string.evening);
+    }
+
+    @Override
+    public String getNight() {
+        return getResources().getString(R.string.night);
     }
 }
